6fb6bd05afdb8a91f4ffd5b36a374e97f70d94ca
vincent_dk (Martin Vincent) <vincent@math.ku.dk>
2014-02-21 18:04:18 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/msgl@93 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/sglOptim/tests/cv_test_3.R
@@ -0,0 +1,25 @@
+library(sglOptim)
+
+data(TestData)
+x <- test.data$x
+y <- test.data$y
+grp <- test.data$grp
+
+weights <- rep(1/nrow(x), nrow(x))
+sampleGrouping <- grp
+covariateGrouping <- factor(1:ncol(x))
+groupWeights <- c(sqrt(length(levels(sampleGrouping))*table(covariateGrouping)))
+parameterWeights <-  matrix(1, nrow = length(levels(sampleGrouping)), ncol = ncol(x))
+alpha <- 1
+d <- 20L
+lambda.min <- 0.5
+algorithm.config <- sgl.standard.config 
+
+# create data
+data <- create.sgldata(x, y, weights, sampleGrouping)
+lambda <- sgl_lambda_sequence("sgl_test_dense", "sglOptim", data, covariateGrouping, groupWeights, parameterWeights, alpha = alpha, d = d, lambda.min, algorithm.config)
+fit1a.cv <- sgl_cv("sgl_test_dense", "sglOptim", data, covariateGrouping, groupWeights, parameterWeights, alpha, lambda, fold = 2L, cv.indices = list(), max.threads = 1L, seed = 331L, algorithm.config)
+
+data <- create.sgldata(x, y, weights, sampleGrouping, sparseX = TRUE)
+fit1b.cv <- sgl_cv("sgl_test_sparse", "sglOptim", data, covariateGrouping, groupWeights, parameterWeights, alpha, lambda, fold = 2L, cv.indices = list(), max.threads = 1L, seed = 331L, algorithm.config)
+
